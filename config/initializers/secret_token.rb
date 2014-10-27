791fbe46c6a22afc7d0ae9c154980e80ddf4c87c
BairaOyd <obairka@gmail.com>
2014-10-27 14:58:01 +0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,27 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+require 'securerandom'
+
+def secure_token
+  token_file = Rails.root.join('.secret')
+  if File.exist?(token_file)
+    # Use the existing token.
+    File.read(token_file).chomp
+  else
+    # Generate a new token and store it in token_file.
+    token = SecureRandom.hex(64)
+    File.write(token_file, token)
+    token
+  end
+end
+
+YandexMap::Application.config.secret_key_base = '2d001531767c53278ca4b5de7d76dc331714ba308d4da7f71c70368374d589ec2e08ce8a6609b0086a071c091d1fd80d5425d3265b70d40bbb75c50c69acc667'
